78ac13a5ad828bfb3129d2d2e41b3ceb8057da27
Diego Juliao <dianjuar@gmail.com>
2020-01-17 06:25:02 -0500

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'to-production-with-just-one-command-talk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo';
+}
